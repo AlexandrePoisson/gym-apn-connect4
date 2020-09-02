4febb911fb638510ae4498177108dcec8c18e06c
Alexandre Poisson <alexandrepoisson@gmail.com>
2020-09-02 21:57:41 +0200

    remove extra hard

--- gym_apn_connect4/__init__.py
+++ gym_apn_connect4/__init__.py
@@ -1,10 +1,11 @@
 from gym.envs.registration import register
 
 register(
-    id='apn_connect4-v0',
-    entry_point='gym_foo.envs:ApnConnect4Env',
+    id='ApnConnect4-v0',
+    entry_point='gym_apn_connect4.envs:ApnConnect4Env',
 )
-register(
-    id='apn_connect4-extrahard-v0',
-    entry_point='gym_apn_connect4.envs:ApnConnect4ExtraHardEnv',
-)
+
+#register(
+#    id='apn_connect4-extrahard-v0',
+#    entry_point='gym_apn_connect4.envs:ApnConnect4ExtraHardEnv',
+#)
