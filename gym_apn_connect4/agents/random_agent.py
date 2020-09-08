234426fb71cffbd7d3f0a8fd33ab021613eafda1
Alexandre Poisson <alexandrepoisson@gmail.com>
2020-09-08 22:05:33 +0200

    add random agent

---
+++ gym_apn_connect4/agents/random_agent.py
@@ -0,0 +1,14 @@
+import argparse
+import sys
+
+import gym
+from gym import wrappers, logger
+
+class RandomAgent(object):
+    """The world's simplest agent!"""
+    def __init__(self, action_space):
+        self.action_space = action_space
+
+    def act(self):
+        return self.action_space.sample()
+
